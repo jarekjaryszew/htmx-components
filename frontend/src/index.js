22346fccd344b7846718d80db5fc93ade747956a
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-05-29 18:11:00 +0200

    Shadow root now inherits the global css.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,38 @@
 import "bulma/css/bulma.min.css";
 import 'htmx.org';
 
-// Component with no shadow root
-class NavMenu extends HTMLElement {
-    constructor() {
+
+class GlobCSSTemplElement extends HTMLElement {
+    constructor(tpl) {
         super();
-        let template = document.getElementById("nav-menu-templ");
+        let template = document.getElementById(tpl);
         let templateContent = template.content;
         const shadowRoot = this.attachShadow({ mode: "open" });
         shadowRoot.appendChild(document.importNode(templateContent, true));
+        const globalStyles = document.querySelectorAll('style'); // or any identifier
+        globalStyles.forEach(style => {
+            shadowRoot.appendChild(style.cloneNode(true));
+        })
+    }
+}
+
+// Component with no shadow root
+class NavMenu extends GlobCSSTemplElement {
+    constructor() {
+        super("nav-menu-templ");
+        const menuElems = this.shadowRoot.querySelectorAll('a');
+        menuElems.forEach(item => item.addEventListener('click', event => this.setActiveMenu(event)));
     }
 
-    setActiveMenu(elementId) {
-        console.log("Clicked elementId")
+    setActiveMenu(event) {
+        const menuItems = this.shadowRoot.querySelectorAll('a');
+        menuItems.forEach(item => item.classList.remove('is-active'));
+        const clickedElement = event.currentTarget;
+        clickedElement.classList.add('is-active');
     }
 }
 
 customElements.define(
     "my-nav-menu", NavMenu
 )
+
