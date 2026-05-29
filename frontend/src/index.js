a9fbb641fa48491b9546494c7a70111222f0040e
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-05-29 16:15:44 +0200

    Using proper htmx package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,21 @@
 import "bulma/css/bulma.min.css";
+import 'htmx.org';
+
+// Component with no shadow root
 class NavMenu extends HTMLElement {
-  constructor() {
-    super();
-  }
-  // Element functionality written in here
+    constructor() {
+        super();
+        let template = document.getElementById("nav-menu-templ");
+        let templateContent = template.content;
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(document.importNode(templateContent, true));
+    }
+
+    setActiveMenu(elementId) {
+        console.log("Clicked elementId")
+    }
 }
+
+customElements.define(
+    "my-nav-menu", NavMenu
+)
