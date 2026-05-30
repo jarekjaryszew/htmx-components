f66f38b24eb1f0613cdec3b593e7efa93843918a
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-05-30 08:52:31 +0200

    Splitting base classes into element with and without shadow

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,33 @@ import "bulma/css/bulma.min.css";
 import htmx from 'htmx.org';
 
 
-class GlobCSSTemplElement extends HTMLElement {
+class TemplElement extends HTMLElement {
     constructor(tpl) {
+        super();
+        let template = document.getElementById(tpl);
+        let templateContent = template.content;
+        this.appendChild(templateContent)
+    }
+
+}
+
+class ShadowTemplElement extends HTMLElement {
+    constructor(tpl, useGlobalStyles) {
         super();
         let template = document.getElementById(tpl);
         let templateContent = template.content;
         const shadowRoot = this.attachShadow({ mode: "open" });
         shadowRoot.appendChild(document.importNode(templateContent, true));
-        const globalStyles = document.querySelectorAll('style'); // or any identifier
-        globalStyles.forEach(style => {
-            shadowRoot.appendChild(style.cloneNode(true));
-        })
+        if (useGlobalStyles) {
+            const globalStyles = document.querySelectorAll('style'); // or any identifier
+            globalStyles.forEach(style => {
+                shadowRoot.appendChild(style.cloneNode(true));
+            })
+        }
+
     }
 
-        connectedCallback() {
+    connectedCallback() {
         // htmx does not auto-scan shadow roots; process this component root explicitly.
         if (htmx && typeof htmx.process === 'function') {
             htmx.process(this.shadowRoot);
@@ -24,15 +37,15 @@ class GlobCSSTemplElement extends HTMLElement {
 }
 
 // Component with no shadow root
-class NavMenu extends GlobCSSTemplElement {
+class NavMenu extends TemplElement {
     constructor() {
         super("nav-menu-templ");
-        const menuElems = this.shadowRoot.querySelectorAll('a');
+        const menuElems = this.querySelectorAll('a');
         menuElems.forEach(item => item.addEventListener('click', event => this.setActiveMenu(event)));
     }
 
     setActiveMenu(event) {
-        const menuItems = this.shadowRoot.querySelectorAll('a');
+        const menuItems = this.querySelectorAll('a');
         menuItems.forEach(item => item.classList.remove('is-active'));
         const clickedElement = event.currentTarget;
         clickedElement.classList.add('is-active');
